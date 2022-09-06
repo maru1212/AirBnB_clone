1399bfcdab6a6415fe13180149d31a488deb36ab
maru1212 <maranataflk9@gmail.com>
2022-09-06 10:16:25 +0300

    airbnb

--- console.py
+++ console.py
@@ -12,10 +12,6 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def emptyline(self, line):
-        """Handles empty lines. """
-        return Cmd.emptyline(self)
-
     def do_quit(self, line):
         """Quit command to exit the program.
         """
@@ -27,7 +23,6 @@ class HBNBCommand(cmd.Cmd):
         Args:
             line (args): argument to for quiting the terminal.
         """
-        print("bye, bye")
         return True
 
 if __name__ == "__main__":
